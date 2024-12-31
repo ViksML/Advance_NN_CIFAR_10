432d93a183f9559a96880521904d460325afc210
Vikas Sharma <vikassharma@Vikass-Mac-mini.local>
2024-12-31 09:16:44 +0530

    README.md update

--- model/model.py
+++ model/model.py
@@ -13,10 +13,12 @@ class CustomCNN(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Depthwise Convolution
             nn.Conv2d(32, 32, kernel_size=3, dilation=2, padding=2, groups=32),  
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Pointwise Convolution 
             nn.Conv2d(32, 32, kernel_size=1), 
             nn.BatchNorm2d(32),
             nn.ReLU()
@@ -24,14 +26,17 @@ class CustomCNN(nn.Module):
 
         # Block 2 
         self.block2 = nn.Sequential(
+            # Depthwise Convolution
             nn.Conv2d(32, 64, kernel_size=3, dilation=2, padding=2, groups=32),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Depthwise Convolution
             nn.Conv2d(64, 64, kernel_size=3, dilation=3, padding=3, groups=64), 
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Pointwise Convolution
             nn.Conv2d(64, 64, kernel_size=1),
             nn.BatchNorm2d(64),
             nn.ReLU()
@@ -39,14 +44,17 @@ class CustomCNN(nn.Module):
 
         # Block 3 
         self.block3 = nn.Sequential(
+            # Depthwise Convolution
             nn.Conv2d(64, 128, kernel_size=3, dilation=3, padding=3, groups=64),  
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Depthwise Convolution
             nn.Conv2d(128, 128, kernel_size=3, dilation=4, padding=4, groups=128),  
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Pointwise Convolution
             nn.Conv2d(128, 128, kernel_size=1), 
             nn.BatchNorm2d(128),
             nn.ReLU()
@@ -62,6 +70,7 @@ class CustomCNN(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout(0.1),
+            # Pointwise Convolution
             nn.Conv2d(256, 256, kernel_size=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
@@ -81,6 +90,5 @@ class CustomCNN(nn.Module):
         x = self.block4(x)
         x = x.view(x.size(0), -1) 
         x = F.relu(self.fc1(x))
-        #x = self.dropout(x)
         x = self.fc2(x)
         return x
